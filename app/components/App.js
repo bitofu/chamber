22a84ac3a87ad44fe52da7a09052f99b702e2ca8
bittertofu <vincent.sungasong@gmail.com>
2016-05-22 21:47:11 -0700

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  };
+};
+
+export default App;
---
+++ app/routes.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import {Route} from 'react-router';
+import App from './components/App';
+import Home from './components/Home';
